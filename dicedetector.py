9e6eb8375f264f181520dbea44cad2c6c8a7bc95
TheMorgus <themorgus@outlook.com>
2020-07-25 19:55:57 -0400

    Add mask color thresholding using GUI

--- dicedetector.py
+++ dicedetector.py
@@ -21,13 +21,14 @@ def removeBackground(image, bgcolor,hadjustlow=10,slow=100,vlow=100,\
 	high_background = np.array([h+hadjusthigh,shigh,vhigh])
 	mask = cv2.inRange(imagehsv, low_background, high_background)
 	mask = cv2.bitwise_not(mask)
+	
+	#Basic filtering and noise reduction
 	kernel = np.ones((5,5),np.uint8)
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)
-	#Basic filtering and noise reduction
 	kernel = np.ones((5,5),np.float32)/25
 	mask = cv2.filter2D(mask,-1,kernel)
-	kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
-	mask=cv2.dilate(mask,kernel)
+	#kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
+	#mask=cv2.dilate(mask,kernel)
 	_,mask=cv2.threshold(mask,1,255,cv2.THRESH_BINARY)
 	res = cv2.bitwise_and(img,img,mask=mask)
 	return res,mask
@@ -42,15 +43,15 @@ def clickColor(event, x, y, flags, param):
 	if event == cv2.EVENT_LBUTTONDOWN:
 		refPt=[(x,y)]
 		print(refPt)
-		print(res[y,x])
+		print(img[y,x])
 
 def isolateDice(diceimage,mainimg):
 	diceimage = cv2.cvtColor(diceimage, cv2.COLOR_BGR2GRAY)
 	_,contours,_=cv2.findContours(diceimage,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	contours2=[]
 	for cnt in contours:
-		#print(cv2.contourArea(cnt))
-		if(cv2.contourArea(cnt)>3000)and(cv2.contourArea(cnt)<4000):
+		print(cv2.contourArea(cnt))
+		if(cv2.contourArea(cnt)>2000)and(cv2.contourArea(cnt)<4000):
 			contours2.append(cnt)
 	warped=[]
 	rects=[]
@@ -84,31 +85,29 @@ def createRotatedTemplates(basetemplate):
 	for x in range(1,4):
 		template.append(rotateImg(basetemplate,90*x))
 	return template
+def resizeImage(img):
+	img = cv2.pyrDown(img)
+	img = cv2.pyrDown(img)
+	return img
 def getTemplates():
 	dicetemplates = []
-	template=cv2.imread('dicetest/white/templateone.jpg')
-	template = cv2.pyrDown(template)
-	template = cv2.pyrDown(template)
+	template=cv2.imread('dicetest/numbers/templateone.jpg')
+	template = resizeImage(template)
 	dicetemplates.append(template)
-	template=cv2.imread('dicetest/white/templatetwo.jpg')
-	template = cv2.pyrDown(template)
-	template = cv2.pyrDown(template)
+	template=cv2.imread('dicetest/numbers/templatetwo.jpg')
+	template = resizeImage(template)
 	dicetemplates.append(template)
-	template=cv2.imread('dicetest/white/templatethree.jpg')
-	template = cv2.pyrDown(template)
-	template = cv2.pyrDown(template)
+	template=cv2.imread('dicetest/numbers/templatethree.jpg')
+	template = resizeImage(template)
 	dicetemplates.append(template)
-	template=cv2.imread('dicetest/white/templatefour.jpg')
-	template = cv2.pyrDown(template)
-	template = cv2.pyrDown(template)
+	template=cv2.imread('dicetest/numbers/templatefour.jpg')
+	template = resizeImage(template)
 	dicetemplates.append(template)
-	template=cv2.imread('dicetest/white/templatefive.jpg')
-	template = cv2.pyrDown(template)
-	template = cv2.pyrDown(template)
+	template=cv2.imread('dicetest/numbers/templatefive.jpg')
+	template = resizeImage(template)
 	dicetemplates.append(template)
-	template=cv2.imread('dicetest/white/templatesix.jpg')
-	template = cv2.pyrDown(template)
-	template = cv2.pyrDown(template)
+	template=cv2.imread('dicetest/numbers/templatesix.jpg')
+	template = resizeImage(template)
 	dicetemplates.append(template)
 	return dicetemplates
 def completeTemplates(templates):
@@ -140,10 +139,86 @@ def drawDiceVals(dicevals,rects,mainimg):
 		FONTCOLOR,
 		LINETYPE)
 		x=x+1
-img = cv2.imread('dicetest/white/white2cropped.jpg', -1)
-img = cv2.pyrDown(img)
-img = cv2.pyrDown(img)
+def nothing(x):
+	pass
+#Function takes the base image of the dice space
+#then provides a GUI that allows the user to calibrate the color thresholding
+#by using a visual representation of the mask used in dice recognition
+def calibrateMask(img,hhigh=5,hlow=15,shigh=255,slow=40,vhigh=40,vlow=255):
+	#HSV color is ideal for masking, as it is resistance to shadow and lighting
+	#effects for filter colors. As a result, image converted to HSV color space
+	#for filtering background colors from dice colors
+	imagehsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	
+	#Creates a blank image (all black)
+	#Size of this image determines the length of the trackbars in trackbar
+	#window
+	blankimg = np.array([0,0,0])
+	row=1
+	col=400
+	blankimg=np.full((row,col,3),blankimg)
+	
+	low_background = np.array([hlow,slow,vlow])
+	high_background = np.array([hhigh,shigh,vhigh])
+	mask = cv2.inRange(imagehsv, low_background, high_background)
+	mask = cv2.bitwise_not(mask)
+	
+	res = cv2.bitwise_and(img,img,mask=mask)
+	
+	cv2.namedWindow('image')
+	cv2.namedWindow('trackbars')
+	cv2.createTrackbar('H-HIGH','trackbars',0,255,nothing)
+	cv2.createTrackbar('H-LOW','trackbars',0,255,nothing)
+	cv2.createTrackbar('S-HIGH','trackbars',0,255,nothing)
+	cv2.createTrackbar('S-LOW','trackbars',0,255,nothing)
+	cv2.createTrackbar('V-HIGH','trackbars',0,255,nothing)
+	cv2.createTrackbar('V-LOW','trackbars',0,255,nothing)
+	cv2.imshow('trackbars',blankimg)
+	
+	while(1):
+		cv2.imshow('image',mask)
 
+		k=cv2.waitKey(1)&0xFF
+		if k==27:
+			break
+		
+		#get current position of trackbars
+		h_high=cv2.getTrackbarPos('H-HIGH','trackbars')
+		h_low=cv2.getTrackbarPos('H-LOW','trackbars')
+		s_high=cv2.getTrackbarPos('S-HIGH','trackbars')
+		s_low=cv2.getTrackbarPos('S-LOW','trackbars')
+		v_high=cv2.getTrackbarPos('V-HIGH','trackbars')
+		v_low=cv2.getTrackbarPos('V-LOW','trackbars')
+		threshold = [(h_low,h_high),
+					 (s_low,s_high),
+					 (v_low,v_high)]
+		
+		#create mask from trackbar thresholds
+		low_background = np.array([h_low,s_low,v_low])
+		high_background = np.array([h_high,s_high,v_high])
+		mask = cv2.inRange(imagehsv, low_background, high_background)
+		mask = cv2.bitwise_not(mask)
+		#Filtering for mask
+		#Exact same as filtering done in algorithim for mask
+		#so result is accurate to alogorithm
+		kernel = np.ones((5,5),np.uint8)
+		mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)
+		kernel = np.ones((5,5),np.float32)/25
+		mask = cv2.filter2D(mask,-1,kernel)
+		
+		_,mask=cv2.threshold(mask,1,255,cv2.THRESH_BINARY)		
+		res = cv2.bitwise_and(img,img,mask=mask)
+		
+	cv2.destroyWindow('image')
+	cv2.destroyWindow('trackbars')	
+	return threshold
+		
+		
+		
+img = cv2.imread('dicetest/numbers/number3cropped.jpg', -1)
+img = resizeImage(img)
+
+colorthreshold=calibrateMask(img)
 refPt = []	
 
 isodiceimg,mask = removeBackground(img,HSV_ORANGE,hadjustlow=10,hadjusthigh=10)
@@ -151,10 +226,15 @@ isodiceimg,mask = removeBackground(img,HSV_ORANGE,hadjustlow=10,hadjusthigh=10)
 dicearray,rects=isolateDice(isodiceimg,img)
 templates = getTemplates()
 templates = completeTemplates(templates)
+#e1=cv2.getTickCount() Measuring performance, for 6 dice takes around 0.12 seconds to analyze an image
 dicevals=discernDice(templates,dicearray,img)
 drawDiceVals(dicevals,rects,img)
+#e2=cv2.getTickCount()
+#time=(e2-e1)/cv2.getTickFrequency()
+#print(time)
 cv2.imshow('image',img)
 
+#For debugging purposes (finding colors based on mouseclick on displayed image)
 cv2.namedWindow("image")
 cv2.setMouseCallback("image",clickColor)
 
