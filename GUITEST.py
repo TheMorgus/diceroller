39d8be70138918efb71c900af53fa797d71b0c15
TheMorgus <themorgus@outlook.com>
2020-07-26 07:33:44 -0400

    add setup window skeleton

--- GUITEST.py
+++ GUITEST.py
@@ -1,14 +1,24 @@
 import tkinter as tk
+from tkinter import filedialog
 import math
 from PIL import Image, ImageTk
 import time
 import dicedetector
 import cv2
 
-text=DIRECTIONSCOLORCAILBRATION = 'Directions: Place a set of dice in the image capture area,\n'+\
+DIRECTIONSCOLORCAILBRATION='Directions: Place a set of dice in the image capture area,\n'+\
 								  'then adjust Hue,Saturation,and Lightness thresholds until:\n'+\
 								  '1)Dice Box is uniform white with little to no black holes\n'+\
 								  '2)Dice Boxes are as closely approximated to a square as possible'
+DIRECTIONSBACKGROUNDCROP='DIRECTIONS: Get a capture of the empty bord space, then\n'+\
+						 'press "Get Threshold Base" button to get new thresholds \n'+\
+						 'for background color filtering. When finished, press "Next"\n'+\
+						 'button, and calibration of threshold values will be finalized.\n'+\
+						 'These values will be used to filter background from dice recognition.'
+#Using the previous thresholding values gathered from the backgroundCropWindow
+#as a base, the user is able to further refine the background filtering with
+#the dice to be tested on the rolling space. This feature is mandatory to ensure
+#a proper dice square is isolated for proper dice recognition algorimth operation
 class colorThresholdingWindow(tk.Frame):
 	def __init__(self, master,baseimg,baseimgpil,maskimg):
 		tk.Frame.__init__(self, master)
@@ -19,12 +29,13 @@ class colorThresholdingWindow(tk.Frame):
 		maskimg=dicedetector.calibrateMask2(self.baseimg)
 		self.maskimg=cv2pil(maskimg)
 		self.thresholdvals= [0,0,0,0,0,0]
-		print(self.thresholdvals)
 		
 		self.upperFrame=tk.Frame(self)
 		self.midFrame=tk.Frame(self)
 		self.lowerFrame=tk.Frame(self)
-		self.upperFrames=[tk.Frame(self.upperFrame),tk.Frame(self.upperFrame),tk.Frame(self.upperFrame)]
+		self.upperFrames=[tk.Frame(self.upperFrame),
+						  tk.Frame(self.upperFrame),
+						  tk.Frame(self.upperFrame)]
 
 		self.headers=[]
 
@@ -37,12 +48,18 @@ class colorThresholdingWindow(tk.Frame):
 		huelabels=[]
 		satlabels=[]
 		lightlabels=[]
-		huelabels.append(tk.Label(self.upperFrames[0],text='HIGH'))
-		huelabels.append(tk.Label(self.upperFrames[0],text='LOW'))
-		satlabels.append(tk.Label(self.upperFrames[1],text='HIGH'))
-		satlabels.append(tk.Label(self.upperFrames[1],text='LOW'))
-		lightlabels.append(tk.Label(self.upperFrames[2],text='HIGH'))
-		lightlabels.append(tk.Label(self.upperFrames[2],text='LOW'))
+		huelabels.append(tk.Label(self.upperFrames[0],
+								  text='HIGH'))
+		huelabels.append(tk.Label(self.upperFrames[0],
+								  text='LOW'))
+		satlabels.append(tk.Label(self.upperFrames[1],
+								  text='HIGH'))
+		satlabels.append(tk.Label(self.upperFrames[1],
+								  text='LOW'))
+		lightlabels.append(tk.Label(self.upperFrames[2],
+								  text='HIGH'))
+		lightlabels.append(tk.Label(self.upperFrames[2],
+								  text='LOW'))
 		self.labels=[]
 		self.labels.append(huelabels)
 		self.labels.append(huelabels)
@@ -50,29 +67,44 @@ class colorThresholdingWindow(tk.Frame):
 		
 		self.trackbars=[]
 		self.trackbars.append(tk.Scale(self.upperFrames[0],
-							  from_=0,to=255,orient='horizontal',
-							  command=self.getNewMask))
+							           from_=0,
+							           to=255,
+							           orient='horizontal',
+							           command=self.getNewMask))
 		self.trackbars.append(tk.Scale(self.upperFrames[0],
-							  from_=0,to=255,orient='horizontal',
-							  command=self.getNewMask))
+									   from_=0,
+									   to=255,
+									   orient='horizontal',
+									   command=self.getNewMask))
 		self.trackbars.append(tk.Scale(self.upperFrames[1],
-							  from_=0,to=255,orient='horizontal',
-							  command=self.getNewMask))
+							           from_=0,
+							           to=255,
+							           orient='horizontal',
+							           command=self.getNewMask))
 		self.trackbars.append(tk.Scale(self.upperFrames[1],
-							  from_=0,to=255,orient='horizontal',
-							  command=self.getNewMask))
+							           from_=0,
+							           to=255,
+							           orient='horizontal',
+							           command=self.getNewMask))
 		self.trackbars.append(tk.Scale(self.upperFrames[2],
-							  from_=0,to=255,orient='horizontal',
-							  command=self.getNewMask))
+									   from_=0,
+									   to=255,
+									   orient='horizontal',
+									   command=self.getNewMask))
 		self.trackbars.append(tk.Scale(self.upperFrames[2],
-							  from_=0,to=255,orient='horizontal',
-							  command=self.getNewMask))
+									   from_=0,
+									   to=255,
+									   orient='horizontal',
+									   command=self.getNewMask))
 		
-		self.directiontext=tk.Label(self.lowerFrame,text=DIRECTIONSCOLORCAILBRATION)
-		#render=ImageTk.PhotoImage(image=self.baseimg)
+		self.directiontext=tk.Label(self.lowerFrame,
+									text=DIRECTIONSCOLORCAILBRATION)
+
 		self.img=[]
-		self.img.append(tk.Label(self.midFrame,image=self.baseimgpil))
-		self.img.append(tk.Label(self.midFrame,image=self.maskimg))
+		self.img.append(tk.Label(self.midFrame,
+								 image=self.baseimgpil))
+		self.img.append(tk.Label(self.midFrame,
+								 image=self.maskimg))
 		self.upperFrame.pack()
 		self.midFrame.pack()
 		self.lowerFrame.pack()
@@ -97,27 +129,242 @@ class colorThresholdingWindow(tk.Frame):
 		self.img[0].pack(side='left')
 		self.img[1].pack(side='right')
 		
-		self.retrievebutton=tk.Button(self.lowerFrame,text='New Capture')
-		self.donebutton=tk.Button(self.lowerFrame,text='Use Selected Values')
+		self.retrievebutton=tk.Button(self.lowerFrame,
+									  text='New Capture')
+		self.donebutton=tk.Button(self.lowerFrame,
+									  text='Use Selected Values')
 		
 		self.directiontext.pack(side='top')
 		self.retrievebutton.pack(side='left')
 		self.donebutton.pack(side='right')
+	#Gets a new mask image based on threshold values set on trackbars.
+	#Called every time the trackbar is updated.
 	def getNewMask(self,_):
 		for x in range(6):
 			self.thresholdvals[x]=self.trackbars[x].get()
 		self.maskimg=dicedetector.calibrateMask2(self.baseimg,
-											self.thresholdvals[0],
-											self.thresholdvals[1],
-											self.thresholdvals[2],
-											self.thresholdvals[3],
-											self.thresholdvals[4],
-											self.thresholdvals[5])
+											     self.thresholdvals[0],
+											     self.thresholdvals[1],
+											     self.thresholdvals[2],
+											     self.thresholdvals[3],
+											     self.thresholdvals[4],
+											     self.thresholdvals[5])
 		self.maskimg=cv2pil(self.maskimg)
 		self.img[1].configure(image=self.maskimg)
 		self.img[1].image=self.maskimg
+#This window has all the features finding a base thresholding solution
+#for background filtering using an image of an empty rolling space.
+#These values can be further refined later in the colorThresholdingWindow
+#using actual dice to ensure solution works in a realistic rolling setting.
+class backgroundCropWindow(tk.Frame):
+	def __init__(self, master,baseimg,backgroundimg):
+		tk.Frame.__init__(self, master)
+		self.master = master
+		
+		self.baseimg=baseimg
+		self.backgroundimg=backgroundimg
+		self.thresholdvals=[[None,None],
+							[None,None],
+							[None,None]]
+		
+		self.upperframe=tk.Frame(self)
+		self.middleframe=tk.Frame(self,
+								  highlightbackground='black',
+								  highlightthickness=2)
+		self.lowerframe=tk.Frame(self)
+		
+		self.img = tk.Label(self.upperframe,
+							image=self.backgroundimg)
+		self.thresholdvars=[[tk.StringVar(),tk.StringVar()],
+							[tk.StringVar(),tk.StringVar()],
+							[tk.StringVar(),tk.StringVar()]]
+		for x in range(3):
+			for y in range(2):
+				self.thresholdvars[x][y].set(str(self.thresholdvals[x][y]))
+		self.thresholdheaders=[[tk.Label(self.middleframe,text='HIGH'),
+								tk.Label(self.middleframe,text='LOW')],
+							   [tk.Label(self.middleframe,text='HUE'),
+								tk.Label(self.middleframe,text='SAT'),
+								tk.Label(self.middleframe,text='LIGHT')]]
+		
+		self.thresholdlabels=[[None,None],#made because you need to construct an array
+							  [None,None],#already populated with values to assign
+							  [None,None]]#values by indexes(which happens next)
+		for x in range(3):
+			for y in range(2):
+				self.thresholdlabels[x][y]=tk.Label(self.middleframe,
+													textvariable=self.thresholdvars[x][y],
+													relief='sunken',
+													width=10)
+		self.directions=tk.Label(self.upperframe,
+								 text=DIRECTIONSBACKGROUNDCROP)
+		self.newcapturebutton=tk.Button(self.lowerframe,
+										text='New Capture')
+		self.getbasethresholdbutton=tk.Button(self.lowerframe,
+										text='Get Threshold Base',
+										command=self.getThresholdValues)
+		self.nextbutton=tk.Button(self.lowerframe,
+								  text='Calibrate Threshold\n(Next)',
+								  state='disabled')
+		
+		self.upperframe.pack()
+		self.middleframe.pack()
+		self.lowerframe.pack()
+		self.img.pack()
+		self.directions.pack()
+		for x in range(2):
+			self.thresholdheaders[0][x].grid(row=0,column=x+1)
+		for x in range(3):
+			self.thresholdheaders[1][x].grid(row=x+1,column=0)	
+		for x in range(3):
+			for y in range(2):
+				self.thresholdlabels[x][y].grid(row=x+1,column=y+1)					
+		self.newcapturebutton.pack(side='left')
+		self.getbasethresholdbutton.pack(side='left')
+		self.nextbutton.pack(side='left')
+	def getThresholdValues(self):
+		temps=[[None,None],
+			   [None,None],
+			   [None,None]]
+		for row in self.baseimg:
+			for col in row:
+				#Parse HUE high/lows
+				if temps[0][0]==None:
+					temps[0][0]=col[0]
+				elif col[0]>temps[0][0]:
+					temps[0][0]=col[0]
+				if temps[0][1]==None:
+					temps[0][1]=col[0]
+				elif col[0]<temps[0][1]:
+					temps[0][1]=col[0]
+				#Parse SAT high/lows
+				if temps[1][0]==None:
+					temps[1][0]=col[1]
+				elif col[1]>temps[1][0]:
+					temps[1][0]=col[1]
+				if temps[1][1]==None:
+					temps[1][1]=col[1]
+				elif col[1]<temps[1][1]:
+					temps[1][1]=col[1]
+				#Parse LIGHT high/lows
+				if temps[2][0]==None:
+					temps[2][0]=col[2]
+				elif col[2]>temps[2][0]:
+					temps[2][0]=col[2]
+				if temps[2][1]==None:
+					temps[2][1]=col[2]
+				elif col[2]<temps[2][1]:
+					temps[2][1]=col[2]
+		for x in range(3):
+			for y in range(2):
+				self.thresholdvals[x][y]=temps[x][y]
+		for x in range(3):
+			for y in range(2):
+				self.thresholdvars[x][y].set(str(self.thresholdvals[x][y])) #update labels with values	
+		self.nextbutton.configure(state='normal') #enables button for next window
+class rollingSetupWindow(tk.Frame):
+	def __init__(self, master):
+		tk.Frame.__init__(self, master)
+		self.master = master
+		
+		self.thresholdingframe=tk.Frame(self)
+		self.dicetemplateframe=tk.Frame(self)
+		self.trialsetupframe=tk.Frame(self)
+		self.checklistframe=tk.Frame(self, 
+									 relief='sunken',
+									 borderwidth=1)
+		self.movementbuttonsframe=tk.Frame(self)
+		
+		self.headers=[tk.Label(self,text='Background Thresholding'),
+					  tk.Label(self,text='Dice Templates'),
+					  tk.Label(self,text='Testing Setup'),
+					  tk.Label(self,text='Checklist to Begin Testing')]
+		for header in self.headers:
+			header.configure(font='Impact 9 underline')
+		self.thresholdingbuttons=[tk.Button(self.thresholdingframe,
+											text='New Solution'),
+								  tk.Button(self.thresholdingframe,
+											text='Load Solution',
+											command=self.loadBackgroundThreshold)]
+		self.dicetemplatebuttons=[tk.Button(self.dicetemplateframe,
+											text='New Templates'),
+								  tk.Button(self.dicetemplateframe,
+											text='Load Templates',
+											command=self.loadTemplates)]
+		self.testingsetupbuttons=[tk.Button(self.trialsetupframe,
+										   text='Trial Setup',
+										   command=self.trialSetup),
+								 tk.Button(self.trialsetupframe,
+										   text='Dice Area Setup',
+										   command=self.areaSetup)]										
+		self.movementbuttons=[tk.Button(self.movementbuttonsframe,
+										 text='Start',
+										 state='disabled'),
+							   tk.Button(self.movementbuttonsframe,
+										 text='Exit')]
+		self.checkboxstate=[tk.IntVar(),tk.IntVar(),tk.IntVar(),tk.IntVar()]								 
+		self.checkboxes=[tk.Checkbutton(self.checklistframe,
+									    state='disabled',
+									    variable=self.checkboxstate[0]),
+						 tk.Checkbutton(self.checklistframe,
+									    state='disabled',
+									    variable=self.checkboxstate[1]),
+						 tk.Checkbutton(self.checklistframe,
+									    state='disabled',
+									    variable=self.checkboxstate[2]),
+						 tk.Checkbutton(self.checklistframe,
+									    state='disabled',
+									    variable=self.checkboxstate[3])]
+		self.checkboxlabels=[tk.Label(self.checklistframe,text='Background Thresholding'),
+							 tk.Label(self.checklistframe,text='Dice Templates'),
+							 tk.Label(self.checklistframe,text='Trial Setup'),
+							 tk.Label(self.checklistframe,text='Area Setup')]
+		self.headers[0].pack()
+		self.thresholdingframe.pack()
+		self.headers[1].pack()
+		self.dicetemplateframe.pack()
+		self.headers[2].pack()
+		self.trialsetupframe.pack()
+		self.headers[3].pack()
+		self.checklistframe.pack()
+		self.movementbuttonsframe.pack()
+		
+		for button in self.thresholdingbuttons:
+			button.pack(side='left')		
+		for button in self.dicetemplatebuttons:
+			button.pack(side='left')
+		for button in self.testingsetupbuttons:
+			button.pack(side='left')
+		for button in self.movementbuttons:
+			button.pack(side='left')
+			
+		for x in range(4):
+			self.checkboxes[x].grid(row=x,column=0)
+			self.checkboxlabels[x].grid(row=x,column=1,sticky='w')
+	def loadBackgroundThreshold(self):
+		self.checkboxes[0].select()	
+		self.parseClearance()
+		path=filedialog.askopenfilename(title='Select File')
+	def loadTemplates(self):
+		self.checkboxes[1].select()	
+		self.parseClearance()
+		path=filedialog.askopenfilename(title='Select File')
+	def trialSetup(self):
+		self.checkboxes[2].select()	
+		self.parseClearance()
+	def areaSetup(self):
+		self.checkboxes[3].select()	
+		self.parseClearance()		
+	def parseClearance(self):
+		clearance=True
+		for checkbox in self.checkboxstate:
+			if checkbox.get()==0:
+				clearance=False
+		if(clearance):
+			self.movementbuttons[0].configure(state='normal')					 
+#Converts the OpenCV image types into an a PIL image
+#so the image can be displayed in tkinter
 def cv2pil(img):
-	
 	new_img=img.copy()
 	if new_img.ndim==2:
 		pass
@@ -127,21 +374,23 @@ def cv2pil(img):
 		new_img=cv2.cvtColor(new_img,cv2.COLOR_BGRA2RGBA)
 	new_img=Image.fromarray(new_img)
 	new_img=ImageTk.PhotoImage(image=new_img)
-	"""
-	new_img=cv2.flip(img,1)
-	new_img=cv2.cvtColor(img,cv2.COLOR_BGR2RGBA)
-	new_img=Image.fromarray(new_img)
-	cv2.imshow('image',img)
-	print(new_img)
-	"""
 	return new_img
 	
 top=tk.Tk()	
-baseimg = cv2.imread('dicetest/white/white2cropped.jpg', -1)
+#baseimg = cv2.imread('dicetest/numbers/number3cropped.jpg', -1)
+baseimg = cv2.imread('dicetest/white/backgroundcrop.jpg', -1)
 baseimg = dicedetector.resizeImage(baseimg)
 img=baseimg.copy()
 img=cv2pil(img)
 
-a=colorThresholdingWindow(top,baseimg,img,img)
+baseimg=cv2.cvtColor(baseimg,cv2.COLOR_BGR2HSV)
+
+#a=colorThresholdingWindow(top,baseimg,img,img)
+#a.pack()
+
+#a=backgroundCropWindow(top,baseimg,img)
+#a.pack()
+
+a=rollingSetupWindow(top)
 a.pack()
 top.mainloop()
