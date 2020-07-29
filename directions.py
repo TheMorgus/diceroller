0b09edde9906224e6e3959ae52196f1e7bf42982
TheMorgus <themorgus@outlook.com>
2020-07-29 06:13:53 -0400

    offload direction to new file

---
+++ directions.py
@@ -0,0 +1,24 @@
+DIRECTIONSCOLORCAILBRATION='Directions: Place a set of dice in the image capture area, and click New Capture.\n'+\
+								  'Then, adjust Hue, Saturation, and Lightness thresholds until:\n'+\
+								  '1)Dice Box is uniform white with little to no black holes\n'+\
+								  '2)Dice Boxes are as closely approximated to a square as possible'
+DIRECTIONSBACKGROUNDCROP='DIRECTIONS: Get a capture of the empty board space, then\n'+\
+						 'press "Get Threshold Base" button to get new thresholds \n'+\
+						 'for background color filtering. When finished, press the "Next"\n'+\
+						 'button, and calibration of threshold values will be finalized.\n'+\
+						 'These values will be used to filter the background for dice recognition.'
+DIRECTIONSTEMPLATECROP="DIRECTIONS\n'Templates' of each of the dice are required by the device,"+\
+					   "including their rotated counterparts.\nTo get the required templates, "+\
+					   "the user must:\n"+\
+					   "1)Crop the dice into a smaller template\n"+\
+					   "2)Further refine the template by rotating,shrinking,and moving the "+\
+					   "image until the dice symbol\n has been properly extracted."+\
+					   "This must be done in order, starting from the dice valued 1\n"+\
+					   "--Press 'Crop Next Template' to begin--"
+DIRECTIONSTEMPLATECROP2="DIRECTIONS\nClick on one corner of the dice to crop, then click on the"+\
+						" opposite corner.\nNOTE:Only the width is taken into account on the second square"+\
+						" to keep\n images square."
+DIRECTIONSTEMPLATECROP3="DIRECTIONS\nThe image on the left is the image before modification, while the"+\
+						" image on the right is the modified template.\nUse the provided buttons to reduce the"+\
+						" space on the modified template so only the symbol and a reasonable\namount of dice"+\
+						" surface is visible, with visible rolling surface."
